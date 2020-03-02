6da032e2e0fb2ba05a342f48dde77cebd1bc34cd
Josh Ellis <joshe523@gmail.com>
2020-03-02 19:22:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to pr
-ogram in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
